8253ca03ff2d089793211476c0fb68c373b5f94a
Datrinon <dtrinh089@gmail.com>
2021-10-29 16:19:10 -0700

    Add classes for elements in Home component

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -8,14 +8,27 @@ function Home() {
   return (
     <div className="Home">
       <section className="landing-splash">
-        <h1>Time for an electrifying ride.</h1>
+        <h1
+          className="slogan"
+        >
+          Time for an electrifying ride.
+        </h1>
         <img 
+          className="banner"
           src={banner}
           alt="A bike alongside a mountain vista."/>
       </section>
-      <section className="">
-        <h1>Our Mission</h1>
-        <p>{intro}</p>
+      <section className="our-mission">
+        <h1 className="mission-header">Our Mission</h1>
+        <div>
+        {intro.split("\n").map(segment => {
+          return (
+            <p className="mission-text">
+              {segment}
+            </p>
+          )
+        })}
+        </div>
       </section>
     </div>
   );
