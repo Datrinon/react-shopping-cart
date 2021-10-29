df1550f81918cfab9dbda0b2f8c4d029e70dd270
Datrinon <dtrinh089@gmail.com>
2021-10-29 16:17:37 -0700

    Revise ProductCategoryList to use private Clickbox component

--- src/js/components/ProductCategoryList.js
+++ src/js/components/ProductCategoryList.js
@@ -1,18 +1,39 @@
 import '../../css/ProductCategoryList.css';
 import { useRouteMatch, Link } from "react-router-dom";
 
+import accSquare from "../../images/accessories-square.jpg"
+import ebikesSquare from "../../images/ebikes-square.jpg"
+
+function ClickBox(props) {
+  return (
+    <div className="click-box">
+      <Link to={props.link}>
+        <img src={props.img}/>
+        <h1>{props.title}</h1>
+      </Link>
+    </div>
+  )
+}
+
 function ProductCategoryList() {
   let match = useRouteMatch();
   
   return (
     <div className="ProductCategoryList">
-      This is the page where you list e-bikes or accessories.
       <ul>
         <li>
-          <Link to={`${match.path}/ebikes`}>E-Bikes</Link>
+          <ClickBox 
+            link={`${match.path}/ebikes`}
+            img={ebikesSquare}
+            title={"E-Bikes"}
+          />
         </li>
         <li>
-          <Link to={`${match.path}/accessories`}>Accessories</Link>
+          <ClickBox 
+            link={`${match.path}/accessories`}
+            img={accSquare}
+            title={"Accessories"}
+          />
         </li>
       </ul>
     </div>
