c350e2e79d1f14d7c1a0d063e345a272d14f6c93
Datrinon <dtrinh089@gmail.com>
2021-10-29 16:12:18 -0700

    rename banner to category banner
    Since we only use that banner in the category pages.

--- src/js/components/CategoryBanner.js
+++ src/js/components/CategoryBanner.js
@@ -1,17 +1,17 @@
-import "../../css/Banner.css";
+import "../../css/CategoryBanner.css";
 
 /**
  * It's a component which displays a visual aid alongside a logo.
  * @param {*} props 
  * @returns 
  */
-function Banner(props) {
+function CategoryBanner(props) {
   return (
     <div className="banner">
-      <img className="banner-image" src={props.img ?? ""} alt="banner"/>
+      <img className="banner-image" src={props.img ?? ""} alt={`banner for ${props.text}`}/>
       <h1 className="banner-text">{props.text}</h1>
     </div>
   );
 }
 
-export default Banner;
+export default CategoryBanner;
