d719dd36c4b8f1e765d03bfdcee4341a6ea467ac
Datrinon <dtrinh089@gmail.com>
2021-10-30 10:39:29 -0700

    Convert mobile navheader to sticky, and change splash banner on home

--- src/data/schpiel.js
+++ src/data/schpiel.js
@@ -1,9 +1,7 @@
 export const intro = `
 Mako Bikes started in a small Californian garage in 1970, but our founders always saw something bigger. Fifty years later, we're on a mission to make the world a healthier, happier place through the power of bicycles.
 
-With the arrival of electric bikes, we at Mako understood that the joyous experience of biking was reborn, and thus set out to provide incredible hospitality to our customers, and change the world by getting more people on bikes.
+With the arrival of electric bikes, we at Mako understood that the joyous experience of biking was reborn.We thus set out to change to world by providing the best experience possible in terms of hospitality, customer service, and e-bikes.
 
-Despite the countless years we've provided outstanding customer service and bicycles, for us, it's never just about those things for us.
-
-It's about getting you closer to the places you were meant to be.
+For us, it's not just about the bike, it's about getting you where you were meant to be.
 `;
