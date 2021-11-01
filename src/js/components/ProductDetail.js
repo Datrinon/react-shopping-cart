a425a92b6d556e0b3862a8cf305fcabac9004bae
Datrinon <dtrinh089@gmail.com>
2021-11-01 06:04:15 -0700

    Handle 404s for Invalid Products, and Case when Product URL accessed directly
    There's more than one way to access the product page, which means we can't take advantage of the state passed through the Link object in the category component. That means we have to fetch the data again. We use useEffect to execute the newly added async function fetchData.
    
    The fact that we have to use async operations to load data means that we need to keep track of the state of loading, so we added the enum PAGE_STATES, which has three states -- ready, loading, and not found.
    
    - Any page accessed through the parent category page will instantly be ready.
    - Any page which is accessed directly through URL instead will be loading initially and then be set to ready after it is finished loading.
    - Any page which is accessed directly through URL, but the URL is incorrect will be set to NOT_FOUND.

--- src/js/components/ProductDetail.js
+++ src/js/components/ProductDetail.js
@@ -3,9 +3,11 @@ import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 
 import "../../css/ProductDetail.css";
 import "../../css/ItemQuantity.css";
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams, useRouteMatch } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import { CartContext, CartDispatch } from "./Router";
+
+import NotFound from './NotFound';
 
 import ImgCarousel from "./ImgCarousel";
 import ItemQuantity from "./ItemQuantity";
@@ -13,15 +15,94 @@ import Utility from "../util/utility";
 
 import { MAX_ITEMS } from "./Router";
 
+const PAGE_STATES = {
+  READY: "ready",
+  LOADING: "loading",
+  NOT_FOUND: "not_found"
+};
+
 function ProductDetail(props) {
+  // related to loading
+  const category = useRouteMatch().path.split("/")[2];
+  const productParam = useParams().productId;
+  const [pageState, setPageState] = useState(PAGE_STATES.LOADING);
+  // reloated to cart
   const dispatch = useContext(CartDispatch);
   const cart = useContext(CartContext);
-
-  let details = useLocation()["state"]["details"];
-  let images = useLocation()["state"]["images"];
-  
   const [itemLimitReached, setItemLimitReached] = useState(false);
 
+  // react router and data
+  const locationState = useLocation()["state"];
+  let [details, setDetails] = useState(null);
+  let [images, setImages] = useState(null);
+
+  useEffect(() => {
+    if (locationState !== undefined) {
+      setDetails(locationState.details);
+      setImages(locationState.images);
+      setPageState(PAGE_STATES.READY);
+    } else {
+      fetchData(productParam, category).then((result) => {
+        if (result === null) {
+          setPageState(PAGE_STATES.NOT_FOUND);
+        } else {
+          setDetails(result.details);
+          setImages(result.images);
+          setPageState(PAGE_STATES.READY);
+        }
+      })
+
+    }
+  }, [category, productParam]);
+
+
+  /**
+   * when the link was navigated to directly thru URL, which occurs when bookmarked.
+   *  we have to fetch the data manually ourselves.
+   */
+  async function fetchData(productID, category) {
+    let db;
+    let details;
+    let images = [];
+
+    // preprocess url parameter
+    productID = productID.toLowerCase();
+
+    // load correct db
+    switch(category) {
+      case "ebikes":
+        db = await import("../../data/ebikes.json");
+        break;
+      case "accessories":
+        db = await import("../../data/accessories.json");
+        break;
+      default: 
+        break;
+    }
+
+    db = db.default;
+    // use parameter and db to identify item details
+    details = db.find(item => item.imgdir === productID);
+
+    // if item not found, retur null.
+    if (details === undefined) {
+      return null;
+    }
+
+    // otherwise, it was, and we can get the images.
+    let r = require.context("../../data/productimages", true, /\.(png|jpe?g|svg)$/);
+
+    r.keys().forEach((item) => {
+      let filepath = item.replace("./", "");
+      let dir = filepath.split("/")[0];
+
+      if (dir === productID) {
+        images.push(r(item)["default"]);
+      }
+    });
+
+    return {details, images};
+  }
 
   function addToCart(ev) {
     console.log("adding to cart...");
@@ -53,39 +134,50 @@ function ProductDetail(props) {
     });
   }
 
-
-  return(
-    <section className="product-details">
-      <h1 className="product-title">{details.brand} {details.name}</h1>
-      <ImgCarousel images={images} />
-      <div className="product-price-desc">
-        <p className="price">${details.price}</p>
-        <form className="cart-form">
-          <ItemQuantity initialQuantity={1} />
-          <div>
-            <button className="add-to-cart" type="submit" onClick={addToCart}>Add to Cart</button>
-            <span className="successful-message">Added! ✔</span>
-          </div>
-          {itemLimitReached && (
-            <div className="error-message">
-              <div className="error-title">
-                <h1><FontAwesomeIcon icon={faExclamationCircle}/> Error: Item Limit Reached!</h1>
-              </div>
-              <p className="error-desc">
-                You may only have a maximum of {MAX_ITEMS} of any item in a cart.
-              </p>
+  switch(pageState) {
+    case PAGE_STATES.READY:
+      return (
+        <section className="product-details">
+        <h1 className="product-title">{details.brand} {details.name}</h1>
+        <ImgCarousel images={images} />
+        <div className="product-price-desc">
+          <p className="price">${details.price}</p>
+          <form className="cart-form">
+            <ItemQuantity initialQuantity={1} />
+            <div>
+              <button className="add-to-cart" type="submit" onClick={addToCart}>Add to Cart</button>
+              <span className="successful-message">Added! ✔</span>
             </div>
-          )}
-        </form>
-        <div className="summary">
-          <h2 className="title">About the {details.name}</h2>
-          <p className="description">
-            {details.summary}
-          </p>
+            {itemLimitReached && (
+              <div className="error-message">
+                <div className="error-title">
+                  <h1><FontAwesomeIcon icon={faExclamationCircle}/> Error: Item Limit Reached!</h1>
+                </div>
+                <p className="error-desc">
+                  You may only have a maximum of {MAX_ITEMS} of any item in a cart.
+                </p>
+              </div>
+            )}
+          </form>
+          <div className="summary">
+            <h2 className="title">About the {details.name}</h2>
+            <p className="description">
+              {details.summary}
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+      );
+    case PAGE_STATES.LOADING:
+      return (
+        <p>Loading...</p>
+      );
+    case PAGE_STATES.NOT_FOUND:
+    default: 
+      return (
+        <NotFound />
+      );
+  }
 }
 
 export default ProductDetail;
