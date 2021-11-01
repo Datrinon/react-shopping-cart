f95db126fa101716b98bf6b98b33160cc4badae6
Datrinon <dtrinh089@gmail.com>
2021-11-01 03:56:27 -0700

    Add CSS for 404 Page

--- src/js/components/NotFound.js
+++ src/js/components/NotFound.js
@@ -1,9 +1,17 @@
 import '../../css/NotFound.css';
 
+import { Link } from 'react-router-dom';
+
 function NotFound() {
   return (
-    <div className="NotFound">
-      TODO -- 404 Page.
+    <div className="not-found">
+      <h1 className="error-code">404</h1>
+      <p className="error-dialog">The page you were looking for was not found on the site!</p>
+      <Link to="/">
+        <div className="return-home-button">
+          Return Home
+        </div>
+      </Link>
     </div>
   );
 }
